4c34c1a91672d349a7b8ad1b068b1658f57537bc
Sean Swanson <seancswan@gmail.com>
2024-02-18 16:49:51 -0800

    Cleaned up some more formatting issues, fixed the font issue where apostrophes were coming in with an additional space character... it was an issue with the font being used. part 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
 import { defineConfig } from "astro/config";
+import markdownIntegration from "@astropub/md";
 
 import tailwind from "@astrojs/tailwind";
+import remarkGfm from "remark-gfm";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), markdownIntegration()],
+  markdown: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [],
+    // syntaxHighlight: 'shiki'
+    // syntaxHighlight: 'prism'
+  },
   experimental: {
     contentCollectionCache: true,
   },
