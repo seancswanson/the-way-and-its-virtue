51243a93ac9ac369da90cb52608cd973bfb0660e
Sean Swanson <seancswan@gmail.com>
2024-04-14 23:12:46 -0700

    Add vite config file for SolidJS SSR support

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { resolve, dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import viteSolid from 'vite-plugin-solid'
+
+const path = fileURLToPath(import.meta.url)
+const root = resolve(dirname(path), 'client')
+
+const plugins = [
+  viteSolid({ ssr: true })
+]
+
+const ssr = {
+  noExternal: ['@solidjs']
+}
+
+export default {
+  root,
+  plugins,
+  ssr
+}
